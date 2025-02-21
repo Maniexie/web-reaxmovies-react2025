13d350e9e49903ab5c33d3a243903e88d8837a7e
Maniexie <artiasifotri@gmail.com>
2025-02-21 16:11:14 +0700

    fix: package fixed

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/web-reaxmovies-react2025",
+  base: "/web-reaxmovies-react2025/",
 });
